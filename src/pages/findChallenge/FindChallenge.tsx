2ea549df697ee2924338453be9cc9d27b2e9d58e
Romain <hercez92@hotmail.fr>
2022-07-28 23:20:33 +0200

    add specialty selection

--- src/pages/findChallenge/FindChallenge.tsx
+++ src/pages/findChallenge/FindChallenge.tsx
@@ -1,5 +1,31 @@
+import { useAppSelector } from "../../app/hooks";
+import { selectChallenge } from "../../features/challenge/challengeSlice";
+
 function FindChallenge() {
-    return <section>Find Challenge</section>;
+    const challenges = useAppSelector(selectChallenge);
+    const specialtiesOptions = challenges
+        .map((challenge) => challenge.specialty)
+        .sort()
+        .map((specialty) => <option>{specialty}</option>);
+
+    return (
+        <section>
+            <div className="form-control w-full">
+                <label className="label">
+                    <span className="label-text font-bold text-xl text-primary">
+                        Find your challenge !
+                    </span>
+                </label>
+                <select
+                    className="select select-primary select-bordered text-primary"
+                    defaultValue="Pick your challenge"
+                >
+                    <option disabled>Pick your challenge</option>
+                    {specialtiesOptions}
+                </select>
+            </div>
+        </section>
+    );
 }
 
 export default FindChallenge;
