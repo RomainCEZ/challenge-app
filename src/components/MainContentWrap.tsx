1f43c349aa167940aeae87727b54a8f3f3dfc365
Romain <hercez92@hotmail.fr>
2022-07-28 22:51:41 +0200

    move navbar to separate component and add findchallenge path

--- src/components/MainContentWrap.tsx
+++ src/components/MainContentWrap.tsx
@@ -1,24 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+import FindChallenge from "../pages/findChallenge/FindChallenge";
 import Leaderboard from "../pages/leaderboard/Leaderboard";
 import NewChallenge from "../pages/newChallenge/NewChallenge";
+import NavBar from "./NavBar";
 
 function MainContentWrap() {
     return (
         <main className="flex flex-col items-center justify-center mt-12 gap-12">
-            <div className="btn-group">
-                <button className="btn btn-primary btn-outline border-primary btn-active">
-                    Leaderboard
-                </button>
-                <button className="btn btn-primary btn-outline border-primary">
-                    New
-                </button>
-                <button className="btn btn-primary btn-outline border-primary">
-                    Find
-                </button>
-            </div>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Leaderboard />} />
                 <Route path="newchallenge" element={<NewChallenge />} />
+                <Route path="findchallenge" element={<FindChallenge />} />
             </Routes>
         </main>
     );
