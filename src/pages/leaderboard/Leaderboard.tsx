1645c2e79e3ed00e039e6c00f733017d5a7cc4ce
Romain <hercez92@hotmail.fr>
2022-07-30 19:15:50 +0200

    change display to grid

--- src/pages/leaderboard/Leaderboard.tsx
+++ src/pages/leaderboard/Leaderboard.tsx
@@ -1,26 +1,35 @@
 import { useAppSelector } from "../../app/hooks";
 import { selectChallenge } from "../../features/challenge/challengeSlice";
 import Challenge from "../../features/challenge/models/Challenge";
-import ChampionCard from "./ChampionCard";
+import ChallengeCard from "../../components/challengeCard/ChallengeCard";
 
 function Leaderboard() {
     const challenges: Challenge[] = [...useAppSelector(selectChallenge)].sort(
-        (a: Challenge, b: Challenge) => b.winstreak - a.winstreak
+        (a: Challenge, b: Challenge) => b.winstreak! - a.winstreak!
+    );
+    const challengeCards: JSX.Element[] = challenges.map(
+        (challenge: Challenge) => (
+            <ChallengeCard
+                key={challenge.id}
+                id={challenge.id}
+                champion={challenge.champion}
+                specialty={challenge.specialty}
+                winstreak={challenge.winstreak}
+                activeChallenge={challenge.activeChallenge}
+                challenger={challenge.challenger}
+            />
+        )
     );
 
-    const champions: JSX.Element[] = challenges.map((challenge: Challenge) => (
-        <ChampionCard
-            key={challenge.id}
-            id={challenge.id}
-            champion={challenge.champion}
-            specialty={challenge.specialty}
-            winstreak={challenge.winstreak}
-        />
-    ));
-
     return (
-        <section className="flex flex-col items-center justify-center p-3 gap-3">
-            {champions.length > 0 ? champions : <p>No champion yet !</p>}
+        <section className="flex flex-col items-center justify-center w-full gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-6xl p-3 sm:px-6 gap-3 w-full">
+                {challengeCards.length > 0 ? (
+                    challengeCards
+                ) : (
+                    <p>No champion yet !</p>
+                )}
+            </div>
         </section>
     );
 }
