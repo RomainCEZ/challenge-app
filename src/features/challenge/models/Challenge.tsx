9e8e9863a3bd3031ecdc40f3c470b224477799bd
Romain <hercez92@hotmail.fr>
2022-07-30 19:18:47 +0200

    create Challenge class

--- src/features/challenge/models/Challenge.tsx
+++ src/features/challenge/models/Challenge.tsx
@@ -1,6 +1,27 @@
-export default interface Challenge {
+import { nanoid } from "nanoid";
+
+interface IChallenge {
+    id?: string;
+    champion: string;
+    specialty: string;
+    winstreak?: number;
+    activeChallenge?: boolean;
+    challenger?: string;
+}
+
+export default class Challenge {
     id: string;
     champion: string;
     specialty: string;
     winstreak: number;
+    activeChallenge: boolean;
+    challenger?: string;
+    constructor(challenge: IChallenge) {
+        this.id = challenge.id || nanoid();
+        this.champion = challenge.champion;
+        this.specialty = challenge.specialty;
+        this.winstreak = challenge.winstreak || 0;
+        this.activeChallenge = challenge.activeChallenge || false;
+        this.challenger = challenge.challenger || "";
+    }
 }
