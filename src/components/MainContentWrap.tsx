5673850f581e5ce7a3abeda414b391b951eadb2e
Romain <hercez92@hotmail.fr>
2022-07-30 19:19:20 +0200

    add NewChallenge Context

--- src/components/MainContentWrap.tsx
+++ src/components/MainContentWrap.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import FindChallenge from "../pages/findChallenge/FindChallenge";
 import Leaderboard from "../pages/leaderboard/Leaderboard";
+import { NewChallengeProvider } from "../pages/newChallenge/context/NewChallengeContext";
 import NewChallenge from "../pages/newChallenge/NewChallenge";
 import NavBar from "./NavBar";
 
@@ -10,7 +11,14 @@ function MainContentWrap() {
             <NavBar />
             <Routes>
                 <Route path="/" element={<Leaderboard />} />
-                <Route path="newchallenge" element={<NewChallenge />} />
+                <Route
+                    path="newchallenge"
+                    element={
+                        <NewChallengeProvider>
+                            <NewChallenge />
+                        </NewChallengeProvider>
+                    }
+                />
                 <Route path="findchallenge" element={<FindChallenge />} />
             </Routes>
         </main>
