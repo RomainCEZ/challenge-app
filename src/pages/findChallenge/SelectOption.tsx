4006163b23839eff6b79da4f50969055a6bf4055
Romain <hercez92@hotmail.fr>
2022-07-31 18:58:48 +0200

    capitalize text

--- src/pages/findChallenge/SelectOption.tsx
+++ src/pages/findChallenge/SelectOption.tsx
@@ -2,6 +2,6 @@ interface OptionProps {
     children: string;
 }
 function SelectOption({ children }: OptionProps) {
-    return <option className="font-bold">{children}</option>;
+    return <option className="font-bold capitalize">{children}</option>;
 }
 export default SelectOption;
