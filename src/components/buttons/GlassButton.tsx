2750093ce83bc375c5e1677b7a525e34dc58c1e2
Romain <hercez92@hotmail.fr>
2022-08-01 23:52:24 +0200

    add disabled property

--- src/components/buttons/GlassButton.tsx
+++ src/components/buttons/GlassButton.tsx
@@ -5,14 +5,22 @@ interface ButtonProps {
     children?: string;
     className?: string;
     handleClick?: MouseEventHandler;
+    disabled?: boolean;
 }
 
-function GlassButton({ id, children, className, handleClick }: ButtonProps) {
+function GlassButton({
+    id,
+    children,
+    className,
+    handleClick,
+    disabled,
+}: ButtonProps) {
     return (
         <button
             id={id}
-            className={`btn btn-ghost glass ring-1 ring-primary border-primary shadow-md hover:ring-2 transition-all ${className}`}
+            className={`btn btn-ghost glass ring-1 ring-primary border-primary shadow-lg hover:shadow-xl hover:ring-2 transition-all text-primary ${className}`}
             onClick={handleClick}
+            disabled={disabled || false}
         >
             {children}
         </button>
