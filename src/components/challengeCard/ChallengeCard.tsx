e3d6ccb3f55f307c906d072db00e35060f5c32bc
Romain <hercez92@hotmail.fr>
2022-08-01 23:57:58 +0200

    add edit and delete card

--- src/components/challengeCard/ChallengeCard.tsx
+++ src/components/challengeCard/ChallengeCard.tsx
@@ -9,24 +9,35 @@ import ActiveChallenge from "./ActiveChallenge";
 import { ChangeEvent, memo, MouseEvent, useState } from "react";
 import InputBorderedPrimary from "../inputs/InputBorderedPrimary";
 import ChallengeCardMenu from "./ChallengeCardMenu";
+import EditCard from "./EditCard";
+import useChallengeValidation from "../validation/useChallengeValidation";
 
 const MemoChallengeCard = memo(ChallengeCard);
 
 function ChallengeCard(props: Challenge) {
     const dispatch = useAppDispatch();
-    const [challenger, setChallenger] = useState("");
+    const [challenger, setChallenger] = useState<string>("");
+    const [editing, setEditing] = useState<boolean>(false);
+    const { errors, testInput, resetInput } = useChallengeValidation();
 
-    function issueChallenge(e: MouseEvent<HTMLFormElement>) {
+    function issueChallenge(e: MouseEvent<HTMLFormElement>): void {
         e.preventDefault();
-        dispatch(challengeChampion({ id: props.id!, challenger }));
+        if (!testInput({ challenger, champion: props.champion })) return;
+        dispatch(
+            challengeChampion({
+                id: props.id!,
+                challenger,
+            })
+        );
         setChallenger("");
     }
 
-    function changeChallenger(e: ChangeEvent<HTMLInputElement>) {
-        setChallenger(e.currentTarget.value);
+    function changeChallenger(e: ChangeEvent<HTMLInputElement>): void {
+        resetInput(e.target.name.toLowerCase());
+        setChallenger(e.target.value);
     }
 
-    function handleWinner(e: MouseEvent<HTMLButtonElement>) {
+    function handleWinner(e: MouseEvent<HTMLButtonElement>): void {
         dispatch(
             declareWinner({
                 id: props.id!,
@@ -35,37 +46,68 @@ function ChallengeCard(props: Challenge) {
         );
     }
 
+    function toggleEdit(): void {
+        setEditing((editing) => !editing);
+    }
+
     return (
-        <article className="flex flex-col justify-between flex-grow border border-primary rounded-xl h-full w-full px-3 py-2 font-bold">
-            <div className=" divide-y divide-primary">
-                <div className="flex justify-between items-center">
-                    <h2 className="flex justify-between text-xl p-1 py-2 capitalize break-words text-primary">
-                        {props.specialty}
-                    </h2>
-                    <ChallengeCardMenu />
-                </div>
-                <div className="p-3 text-lg">
-                    <p className="break-words">Champion : {props.champion} </p>
-                    <p>Challenges won : {props.winstreak}</p>
-                </div>
-            </div>
-            {props.challenger ? (
-                <ActiveChallenge
-                    handleClick={handleWinner}
-                    challenger={props.challenger!}
-                />
+        <article className="flex flex-col justify-between flex-grow border border-primary shadow-lg rounded-xl h-full w-full px-3 py-2 font-bold">
+            {editing ? (
+                <EditCard id={props.id} toggleEdit={toggleEdit} />
             ) : (
-                <form
-                    className="flex flex-col justify-end m-3"
-                    onSubmit={issueChallenge}
-                >
-                    <InputBorderedPrimary
-                        value={challenger}
-                        name={"Challenger"}
-                        handleChange={changeChallenger}
-                    />
-                    <GlassButton className="btn-sm">Challenge</GlassButton>
-                </form>
+                <>
+                    <div className="divide-y divide-primary">
+                        <div className="flex justify-between items-center">
+                            <h2 className="flex justify-between text-2xl p-1 py-2 capitalize break-words text-primary">
+                                {props.specialty}
+                            </h2>
+                            <ChallengeCardMenu
+                                id={props.id}
+                                challenge={props.specialty}
+                                toggleEdit={toggleEdit}
+                            />
+                        </div>
+                        <div className="flex flex-col justify-center items-center pt-4 px-3 gap-1">
+                            <p className="break-words text-3xl">
+                                {props.champion}
+                            </p>
+                            <p className="text-2xl">
+                                {props.winstreak}{" "}
+                                {props.winstreak > 1 ? "wins" : "win"}
+                            </p>
+                        </div>
+                    </div>
+                    {props.challenger ? (
+                        <ActiveChallenge
+                            handleClick={handleWinner}
+                            challenger={props.challenger!}
+                        />
+                    ) : (
+                        <div>
+                            <p className="text-lg text-center mt-5">
+                                Issue your challenge :
+                            </p>
+                            <form
+                                className="flex flex-col justify-end m-3"
+                                onSubmit={issueChallenge}
+                            >
+                                <InputBorderedPrimary
+                                    value={challenger}
+                                    name={"Challenger"}
+                                    placeholder="Challenger name *"
+                                    handleChange={changeChallenger}
+                                    error={errors.challenger}
+                                />
+                                <GlassButton
+                                    className="btn-sm"
+                                    disabled={challenger.length < 3}
+                                >
+                                    I Challenge You !
+                                </GlassButton>
+                            </form>
+                        </div>
+                    )}
+                </>
             )}
         </article>
     );
