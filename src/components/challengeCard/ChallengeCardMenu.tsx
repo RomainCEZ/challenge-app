3e92a1af2ec4b370c0fdba5a09fdf7b478542ef1
Romain <hercez92@hotmail.fr>
2022-08-01 23:57:16 +0200

    add edit and delete challenge

--- src/components/challengeCard/ChallengeCardMenu.tsx
+++ src/components/challengeCard/ChallengeCardMenu.tsx
@@ -1,9 +1,22 @@
+import { MouseEventHandler } from "react";
 import { BsThreeDots } from "react-icons/bs";
+import { useAppDispatch } from "../../app/hooks";
+import { removeChallenge } from "../../features/challenge/challengeSlice";
+import ConfirmModal from "../modals/ConfirmModal";
 
-function ChallengeCardMenu() {
+interface MenuProps {
+    id: string;
+    challenge: string;
+    toggleEdit: MouseEventHandler<HTMLButtonElement>;
+}
+
+function ChallengeCardMenu({ id, challenge, toggleEdit }: MenuProps) {
+    const dispatch = useAppDispatch();
+
     function deleteChallenge() {
-        console.log("delete");
+        dispatch(removeChallenge(id));
     }
+
     return (
         <div className="dropdown dropdown-end items-center">
             <label
@@ -15,17 +28,28 @@ function ChallengeCardMenu() {
             </label>
             <ul
                 tabIndex={0}
-                className="dropdown-content justify-center items-center mt-2 shadow"
+                className="dropdown-content justify-center items-center shadow-lg bg-base-100 rounded-lg"
             >
-                <li>
-                    <button
-                        className="btn btn-sm btn-outline btn-primary capitalize"
-                        onClick={deleteChallenge}
-                    >
-                        Delete
+                <li className="flex btn btn-sm btn-outline rounded-b-none border-none btn-primary w-full">
+                    <button className="capitalize w-full" onClick={toggleEdit}>
+                        Edit
                     </button>
                 </li>
+                <li className="flex btn modal-button btn-sm btn-outline rounded-t-none border-none btn-primary w-full">
+                    <label
+                        htmlFor={`${id}confirmModal`}
+                        className="cursor-pointer capitalize w-full"
+                    >
+                        Delete
+                    </label>
+                </li>
             </ul>
+            <ConfirmModal
+                id={`${id}confirmModal`}
+                headingText={`Do you really want to delete the ${challenge} challenge ?`}
+                buttonText="Delete"
+                handleClick={deleteChallenge}
+            />
         </div>
     );
 }
