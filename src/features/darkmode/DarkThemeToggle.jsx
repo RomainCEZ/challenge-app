3140f0fe6a93c2b389f55f6645d00d2f04538ffb
Romain <hercez92@hotmail.fr>
2022-08-02 00:38:19 +0200

    add icons

--- src/features/darkmode/DarkThemeToggle.jsx
+++ src/features/darkmode/DarkThemeToggle.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
+import { BiMoon } from "react-icons/bi";
+import { MdLightMode } from "react-icons/md";
 
 function DarkThemeToggle() {
     const darkModeActive = localStorage.getItem("theme") === "darkTheme";
@@ -9,16 +11,26 @@ function DarkThemeToggle() {
 
     return (
         <div className="absolute right-0 flex items-center">
-            <div className="m-5" data-toggle-theme="lightTheme,darkTheme">
+            <div
+                className="m-2 sm:m-5"
+                data-toggle-theme="lightTheme,darkTheme"
+            >
                 <label
-                    className="flex label cursor-pointer"
+                    className="flex label cursor-pointer font-bold text-2xl text-primary"
                     aria-label="toggle darkmode"
                 >
+                    <span className="">
+                        <MdLightMode />
+                    </span>
                     <input
                         type="checkbox"
-                        className="toggle toggle-primary"
+                        className="toggle toggle-primary mx-1"
                         defaultChecked={darkModeActive}
                     />
+
+                    <span className="">
+                        <BiMoon />
+                    </span>
                 </label>
             </div>
         </div>
